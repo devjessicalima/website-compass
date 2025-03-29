787db2ef3bccdf09e12eefed463ae6c7b4f5db15
Jéssica Lima <devjessicalima@gmail.com>
2025-03-28 23:25:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const donateBtn = document.getElementById('donateBtn');
+
+    donateBtn.addEventListener('click', () => {
+        alert('Obrigado por considerar fazer uma doação! Visite nossa página de doações para mais informações.');
+    });
+});
